37b6a784b4eec3735b5d463c860742d2aff42e22
Alexandr Sokhryakov <asv7780@gmail.com>
2017-12-25 09:59:35 +0200

    origin file

--- homework/js/custom.js
+++ homework/js/custom.js
@@ -1,5 +1,18 @@
 "use strict"
 
+
+$ (function () {
+    $ ('.persons').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 2000,
+    });
+});
+
+
+
 $ (".facts-counter").each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
@@ -11,3 +24,16 @@ $ (".facts-counter").each(function () {
         }
     });
 });
+
+
+function initMap() {
+    var centerLatLng = new google.maps.LatLng(49.423631, 26.986471);
+    var mapOptions = {
+        center: centerLatLng,
+        zoom: 16
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+google.maps.event.addDomListener(window, "load", initMap);

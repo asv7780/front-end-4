fc60ccb6818fbfe596ed4ed2c45e74683efe9400
Alexandr Sokhryakov <asv7780@gmail.com>
2017-12-27 12:57:42 +0200

    origin file

--- homework/js/custom.js
+++ homework/js/custom.js
@@ -1,8 +1,8 @@
 "use strict"
 
 
-$ (function () {
-    $ ('.persons').slick({
+$(function () {
+    $('.persons').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
@@ -21,33 +21,52 @@ $ (function () {
     });
 });
 
-
-$ (".facts-counter").each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 2000,
-        easing: 'swing',
-        step:function(now) {
-           $(this).text(Math.ceil(now));
+var time = 2, cc = 1;
+$(window).scroll(function () {
+    $('#counter').each(function () {
+        var
+            cPos = $(this).offset().top,
+            topWindow = $(window).scrollTop();
+        if (cPos < topWindow + 200) {
+            if (cc < 2) {
+                $('.facts-counter').addClass('viz');
+                $('div').each(function () {
+                    var
+                        i = 1,
+                        num = $(this).data('num'),
+                        step = 1000 * time / num,
+                        that = $(this),
+                        int = setInterval(function () {
+                            if (i <= num) {
+                                that.html(i);
+                            }
+                            else {
+                                cc = cc + 2;
+                                clearInterval(int);
+                            }
+                            i++;
+                        }, step);
+                });
+            }
         }
     });
 });
 
-$ (function () {
-    $ ("form[name='userForm']").validate({
-        rules :{
-            user :{
+
+$(function () {
+    $("form[name='userForm']").validate({
+        rules: {
+            user: {
                 required: true
             },
-            Email :{
+            Email: {
                 required: true
             },
-            sms :{
+            sms: {
                 required: true
             }
         },
-        messages:{
+        messages: {
             user: "Please, enter your name",
             Email: "Please, enter your Email",
             sms: "Please, enter your text"
